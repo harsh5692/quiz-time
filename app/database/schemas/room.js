1892a48757d196a6c225707125ad399c55675d09
harsh5692 <harsh5692@gmail.com>
2017-08-08 10:54:58 +0530

    moving settings variables above in code

--- app/database/schemas/room.js
+++ app/database/schemas/room.js
@@ -2,6 +2,8 @@
 
 var Mongoose = require('mongoose');
 
+var DEFAULT_ROUNDS = 5;
+var DEFAULT_PLAYERS = 2;
 /**
  * Each connection object represents a user connected through a unique socket.
  * Each connection object composed of {userId + socketId}. Both of them together are unique.
@@ -12,9 +14,9 @@ var RoomSchema = new Mongoose.Schema({
   connections: { type: [{ userId: String, socketId: String }] },
   isOpen: { type: Boolean, default: true },
   owner: { type: { userId: String, socketId: String } },
-  rounds: { type: Number, default: 5 },
+  rounds: { type: Number, default: DEFAULT_ROUNDS },
   currentRound: { type: Number, default: 0 },
-  noOfPlayers: { type: Number, default: 2 },
+  noOfPlayers: { type: Number, default: DEFAULT_PLAYERS },
   createdAt: { type: Date, default: Date.now },
   // questions: { type: Array }
   score: {
